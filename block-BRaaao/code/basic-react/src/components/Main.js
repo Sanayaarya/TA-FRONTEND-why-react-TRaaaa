8ada22bd153fb6887dc8b4d7bf303a9fdcbc2faf
Sanaya Arya <sanayaarya0123@gmail.com>
2022-10-19 06:53:13 +0530

    Assignment: Articles App

---
+++ block-BRaaao/code/basic-react/src/components/Main.js
@@ -0,0 +1,26 @@
+import data from "../data";
+
+function Main() {
+    return (
+        <div className="main">
+            {data.map((article) => {
+                return <Card key={article.title} {...article} />
+            })}
+        </div>
+    );
+}
+
+function Card(props) {
+    return (
+        <div className="wrapper">
+            <img src={props.urlToImage} alt="" width="100%" height="200px" />
+            <h2>Author: {props.author}</h2>
+            <p>Title: {props.title}</p>
+            <p>Description: {props.description}</p>
+            <p>Published At: {props.publishedAt}</p>
+            <p>Content: {props.content}</p>
+        </div>
+    );
+}
+
+export default Main;
