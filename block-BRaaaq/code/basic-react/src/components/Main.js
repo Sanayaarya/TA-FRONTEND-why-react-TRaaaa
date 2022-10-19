e5c3774c2a60dbaca928a8f915e499c460b6e0d4
Sanaya Arya <sanayaarya0123@gmail.com>
2022-10-19 11:12:57 +0530

    Assignment: PropTypes to Articles App

---
+++ block-BRaaaq/code/basic-react/src/components/Main.js
@@ -0,0 +1,14 @@
+import data from "../data";
+import card from "./card";
+
+function Main (){
+    return (
+        <div className="main">
+            {data.map((article) => {
+                return <Card key={article.title} info={article} />
+            })}
+        </div>
+    );
+}
+
+export default Main;
