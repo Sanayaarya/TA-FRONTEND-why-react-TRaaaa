d13867afe6aa27318413b2f5415346b6a4033797
Sanaya Arya <sanayaarya0123@gmail.com>
2022-10-21 12:54:51 +0530

    Done

--- block-BRaaao/code/basic-react/src/components/Main.js
+++ block-BRaaao/code/basic-react/src/components/Main.js
@@ -7,14 +7,14 @@ function Main() {
                 return <Card key={article.title} {...article} />
             })}
         </div>
-    );
+    )
 }
 
 function Card(props) {
     return (
         <div className="wrapper">
             <img src={props.urlToImage} alt="" width="100%" height="200px" />
-            <h2>Author: {props.author}</h2>
+            <h2>Author: {props.author.toUpperCase()}</h2>
             <p>Title: {props.title}</p>
             <p>Description: {props.description}</p>
             <p>Published At: {props.publishedAt}</p>
