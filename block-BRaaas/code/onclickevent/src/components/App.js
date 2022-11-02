29b19fbcefde9fc4dd27272591fa42c84ec0ac8c
Sanaya Arya <sanayaarya0123@gmail.com>
2022-11-02 12:52:51 +0530

    Assignments: onClick Events

---
+++ block-BRaaas/code/onclickevent/src/components/App.js
@@ -0,0 +1,22 @@
+
+
+import Hero from './Hero';
+import Greet from './Greet';
+import Help from './Help';
+import Fruit from './Fruit';
+
+function App() {
+    return (
+        <>
+            <Hero />
+            <Help />
+            <Greet />
+            <Greet />
+            <Fruit />
+        </>
+
+
+    );
+}
+
+export default App;
